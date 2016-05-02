35feb9b79f8e7e16b4e512109f1f1d728752dd1d
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-05-02 11:18:49 +0100

    add basic tests

---
+++ runtest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e -u -x
+
+PRIVILEGED=""
+VOLUMES=""
+# start docker container
+if [[ "darwin" == "${OSTYPE//[0-9.]/}" ]]; then
+    PRIVILEGED="--privileged"
+fi
+
+make start PORTS=$PRIVILEGED
+docker inspect -f {{.State.Running}} omero-ssh-daemon-c7
+
+# CLEANUP
+make stop
+make rm
